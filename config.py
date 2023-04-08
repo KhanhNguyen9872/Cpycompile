bb4b367eef985161f4652554b20602e23aeacca9
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2023-04-08 12:57:13 +0700

    update

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
+## Config
+
+# client.exe
+ip_client="127.0.0.1"
+port_client=12345
+max_time_client=300
+
+# Cpycompile Server
 python_name="Python38-32"
 python_dll="Cpycompile.exe"
 python_lib="python38.lib"
 version_vs=["Community","Professional","Enterprise"]
 arch="x86"
-host="0.0.0.0"
-port=12345
+host_server="0.0.0.0"
+port_server=12345
 max_conn=30
 is_multi=0
 max_size = 307200
